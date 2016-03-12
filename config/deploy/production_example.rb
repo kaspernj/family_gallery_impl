cfa1ee45a33dea7868e1580a0588f28513d68f8f
kaspernj <k@spernj.org>
2016-03-12 11:04:05 +0100

    Fixed example file and added real deploy production file to gitignore

--- config/deploy/production_example.rb
+++ config/deploy/production_example.rb
@@ -4,9 +4,9 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-role :app, %w(deploy@familygallery.kaspernj.org:57022)
-role :web, %w(deploy@familygallery.kaspernj.org:57022)
-role :db,  %w(deploy@familygallery.kaspernj.org:57022)
+role :app, %w(deploy@example.com)
+role :web, %w(deploy@example.com)
+role :db,  %w(deploy@example.com)
 
 
 # Extended Server Syntax
@@ -15,7 +15,7 @@ role :db,  %w(deploy@familygallery.kaspernj.org:57022)
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server "familygallery.kaspernj.org:57022", user: "deploy", roles: %w(web app)
+server "example.com", user: "deploy", roles: %w(web app)
 
 
 # Custom SSH Options
